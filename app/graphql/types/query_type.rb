b90337c18976b9047e824139ae9134eed9ee6166
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-02 20:03:16 +0530

    Add post and comment graphql types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -20,6 +20,7 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :post, resolver: Resolvers::PostResolver
 
     # TODO: remove me
     field :test_field, String, null: false,
