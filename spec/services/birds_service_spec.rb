db0760b41059e0e478c72709580246caf1acf967
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-07 19:49:47 +0530

    spec: test limit for birds endpoint

--- spec/services/birds_service_spec.rb
+++ spec/services/birds_service_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe BirdsService do
   it "gets list of birds" do
     expect(subject.birds.size).to be > 0
   end
+
+  it "gets a list of birds based on limit" do
+    expect(subject.birds(limit: 5).size).to be(5)
+  end
 end
