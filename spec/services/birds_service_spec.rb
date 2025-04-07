4d509a099feabf0b4f30d9d3238618a754d585bd
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-07 19:51:05 +0530

    spec: test bird(id) endpoint

--- spec/services/birds_service_spec.rb
+++ spec/services/birds_service_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe BirdsService do
   it "gets a list of birds based on limit" do
     expect(subject.birds(limit: 5).size).to be(5)
   end
+
+  it "gets a bird based on id" do
+    expect(subject.bird(1).dig("id")).to be(1)
+  end
 end
