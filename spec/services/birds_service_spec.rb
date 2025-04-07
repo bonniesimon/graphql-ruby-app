f10141508989e774d3d24725dfd6e6e417428851
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-07 19:48:30 +0530

    spec: update how birds endpoint is tested

--- spec/services/birds_service_spec.rb
+++ spec/services/birds_service_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe BirdsService do
   let(:subject) {BirdsService.new}
 
   it "gets list of birds" do
-    expect(subject.birds(limit:5).size).to be(5)
+    expect(subject.birds.size).to be > 0
   end
 end
