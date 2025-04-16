56d970c11c55595ed91efe73d4d2f94c75ed70af
Bonnie Simon <bonnie.developer@gmail.com>
2025-04-16 14:01:35 +0530

    add view increment logic

--- app/graphql/resolvers/post_resolver.rb
+++ app/graphql/resolvers/post_resolver.rb
@@ -5,7 +5,18 @@ module Resolvers
 
     # How do we get the comments in the graphiql
     def resolve(id:)
-      ::Post.find(id)
+      handle_view_increment(id)
+      post(id)
     end
+
+    private
+
+      def handle_view_increment(id)
+        Rails.cache.increment("#{post(id).id}:views")
+      end
+
+      def post(id)
+        Post.find(id)
+      end
   end
 end
